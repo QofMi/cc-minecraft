3c4369a3f0b011ea611ee8305a53828a57cd4f16
Иван Шипачев <61776952+QofMi@users.noreply.github.com>
2024-01-22 21:18:24 +0700

    adde try-catch

--- ae_bridge.lua
+++ ae_bridge.lua
@@ -1,19 +1,31 @@
 require("gui")
+require("try-catch")
+
 local ME_BRIDGE = peripheral.find("meBridge")
 
 guiClear()
 
 while true do
-    guiProgressBar(
-        2,
-        5,
-        "Storage",
-        display_x-2,
-        3,
-        ME_BRIDGE.getUsedItemStorage(),
-        ME_BRIDGE.getTotalItemStorage(),
-        colors.green,
-        colors.gray
-    )
-    sleep(0.75)
+    try{
+        function()
+            guiProgressBar(
+                2,
+                5,
+                "Storage",
+                display_x-2,
+                3,
+                ME_BRIDGE.getUsedItemStorage(),
+                ME_BRIDGE.getTotalItemStorage(),
+                colors.green,
+                colors.gray
+            )
+            sleep(0.75)
+        end,
+        catch{
+            function(error)
+                guiClear()
+                os.reboot()
+            end
+        }
+    }
 end
