db4cc250f743f9cd7823f19ceb16c3df3743fdec
Иван Шипачев <61776952+QofMi@users.noreply.github.com>
2024-01-24 01:57:57 +0700

    player-detector

--- player-detector.lua
+++ player-detector.lua
@@ -8,52 +8,40 @@ REDSTONE_SIDE = ""
 USE_WHITE_LIST = false
 WHITE_LIST = {}
 
+local function playerDetectorAction(display_action, redstone_action)
+    if display then
+        if display_action == "draw" then
+            guiDrawText(2, y_pos, player, colors.pink, colors.black)
+            y_pos = y_pos + 1
+        elseif display_action == "clear" then
+            guiClear()
+        end
+    end
+
+    if REDSTONE_SIGNAL then
+        redstone.setOutput(REDSTONE_SIDE, redstone_action)
+    end
+end
+
 guiClear()
 while true do
-    
-    if display then
-        y_pos = 1
-    end
+    y_pos = 1
     
     if PLAYER_DETECTOR then
         for _, player in pairs(PLAYER_DETECTOR.getOnlinePlayers()) do
             if PLAYER_DETECTOR.isPlayerInRange(RANGE_DETECTION, player) then
 
                 if USE_WHITE_LIST then
-
                     if is_contains(WHITE_LIST, player) then
-                        if display then
-                            guiDrawText(2, y_pos, player, colors.pink, colors.black)
-                            y_pos = y_pos + 1
-                        end
-        
-                        if REDSTONE_SIGNAL then
-                            redstone.setOutput(REDSTONE_SIDE, true)
-                        end
+                        playerDetectorAction("draw", true)
                     end
-
                 else
-                    
-                    if display then
-                        guiDrawText(2, y_pos, player, colors.pink, colors.black)
-                        y_pos = y_pos + 1
-                    end
-    
-                    if REDSTONE_SIGNAL then
-                        redstone.setOutput(REDSTONE_SIDE, true)
-                    end
-                
+                    playerDetectorAction("draw", true)
                 end
 
 
             else
-                if display then
-                    guiClear()
-                end
-                
-                if REDSTONE_SIGNAL then
-                    redstone.setOutput(REDSTONE_SIDE, false)
-                end
+                playerDetectorAction("clear", false)
             
             end
         end
