47e981cf259b394300fe75d5f65fd2f99d73ac5d
Иван Шипачев <61776952+QofMi@users.noreply.github.com>
2024-01-24 01:22:34 +0700

    player-detector

--- player-detector.lua
+++ player-detector.lua
@@ -3,17 +3,42 @@ require("gui")
 
 PLAYER_DETECTOR = peripheral.find("playerDetector")
 RANGE_DETECTION = 10
+REDSTONE_SIGNAL = false
+REDSTONE_SIDE = ""
 
-
+guiClear()
 while true do
+    
+    if display then
+        local y_pos = 0
+        --guiClear()
+    end
+    
     if PLAYER_DETECTOR then
         for _, player in pairs(PLAYER_DETECTOR.getOnlinePlayers()) do
             if PLAYER_DETECTOR.isPlayerInRange(RANGE_DETECTION, player) then
-                display.write(("%s\z"):format(player))
+                if display then
+                    guiDrawText(2, y_pos, player, colors.pink, colors.black)
+                end
+
+                if REDSTONE_SIGNAL then
+                    redstone.setOutput(REDSTONE_SIDE, true)
+                end
+
+            else
+                if display then
+                    guiClear()
+                end
+                
+                if REDSTONE_SIGNAL then
+                    redstone.setOutput(REDSTONE_SIDE, false)
+                end
+            
             end
         end
-        sleep(3)
+        sleep(1)
     else
         error("Player detector is not found")
     end
+
 end
