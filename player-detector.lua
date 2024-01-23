3bc63696368258861c027adc0852a075b1d599b2
Иван Шипачев <61776952+QofMi@users.noreply.github.com>
2024-01-24 02:30:41 +0700

    playerDetector

--- player-detector.lua
+++ player-detector.lua
@@ -2,11 +2,28 @@ require("gui")
 
 
 PLAYER_DETECTOR = peripheral.find("playerDetector")
+
+USE_DETECTOR = "by_range" -- by_range | by_coords
 RANGE_DETECTION = 10
+COORDS_DETECTION_POS_1 = {x = nil, y = nil, z = nil}
+COORDS_DETECTION_POS_2 = {x = nil, y = nil, z = nil}
+
 REDSTONE_SIGNAL = false
 REDSTONE_SIDE = ""
+
 USE_WHITE_LIST = false
 WHITE_LIST = {}
+
+
+local function isPlayerDetected()
+    if USE_DETECTOR == "by_range" then
+        return PLAYER_DETECTOR.isPlayerInRange(RANGE_DETECTION, player)
+    end
+    if USE_DETECTOR == "by_coords" then
+        return PLAYER_DETECTOR.isPlayerInCoords(COORDS_DETECTION_POS_1, COORDS_DETECTION_POS_2, player)
+    end
+end
+
 
 local function playerDetectorAction(display_action, redstone_action)
     if display then
@@ -23,6 +40,7 @@ local function playerDetectorAction(display_action, redstone_action)
     end
 end
 
+
 guiClear()
 while true do
     y_pos = 1
@@ -31,7 +49,7 @@ while true do
         for _, _player in pairs(PLAYER_DETECTOR.getOnlinePlayers()) do
             player = _player
             
-            if PLAYER_DETECTOR.isPlayerInRange(RANGE_DETECTION, player) then
+            if isPlayerDetected() then
 
                 if USE_WHITE_LIST then
                     if is_contains(WHITE_LIST, player) then
