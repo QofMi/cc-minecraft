e29419d509e406f76eae222ca5706b1df7086e6b
Иван Шипачев <61776952+QofMi@users.noreply.github.com>
2024-01-25 21:41:20 +0700

    fixed

--- ae_bridge.lua
+++ ae_bridge.lua
@@ -1,37 +1,98 @@
 require("gui")
---require("try-catch")
+require("isa")
 
 local ME_BRIDGE = peripheral.find("meBridge")
+local display = peripheral.find("monitor")
+local display_x, display_y = display.getSize()
+local StorageData = {
+    x = 2,
+    y = 8,
+    name = "Storage",
+    size = 3,
+    length = display_x - 2,
+}
+local StorageColors = {
+    background_color = colors.gray,
+    text_color = colors.black,
+    levels = {
+        {
+            percent = 70,
+            color = colors.green
+        },
+        {
+            percent = 90,
+            color = colors.orange
+        },
+        {
+            percent = 100,
+            color = colors.red
+        }
+    }
+}
+local AEEnergyData = {
+    x = 2,
+    y = 3,
+    name = "AE Energy",
+    size = 3,
+    length = display_x - 2,
+}
+local AEEnergyColors = {
+    background_color = colors.gray,
+    text_color = colors.black,
+    levels = {
+        {
+            percent = 30,
+            color = colors.red
+        },
+        {
+            percent = 60,
+            color = colors.orange
+        },
+        {
+            percent = 100,
+            color = colors.green
+        }
+    }
+}
 
-guiClear()
+function offline(display, data, gui_colors)
+    local name = ("%s is offline"):format(data.name)
+    guiDrawLine(display, data.x, data.y, data.length, data.size, gui_colors.background_color)
+    guiDrawText(display, display_x / 2 - #name / 2 + 2, data.y + data.size / 2, name, colors.black, gui_colors.background_color)
+end
 
+guiClear(display)
 while true do
-    guiProgressBar(
-        2,
-        8,
-        "Storage",
-        display_x-2,
-        3,
-        ME_BRIDGE.getUsedItemStorage(),
-        ME_BRIDGE.getTotalItemStorage(),
-        colors.green,
-        colors.orange,
-        colors.red,
-        colors.gray
-    )
+    if not is_none(ME_BRIDGE.getMaxEnergyStorage()) then
+        guiProgressBar(
+                display,
+                2,
+                3,
+                "AE Energy",
+                display_x - 2,
+                3,
+                ME_BRIDGE.getEnergyStorage(),
+                ME_BRIDGE.getMaxEnergyStorage(),
+                AEEnergyColors
+        )
+    else
+        offline(display, AEEnergyData, AEEnergyColors)
+    end
 
-    guiProgressBar(
-        2,
-        3,
-        "AE Energy",
-        display_x-2,
-        3,
-        ME_BRIDGE.getEnergyStorage(),
-        ME_BRIDGE.getMaxEnergyStorage(),
-        colors.red,
-        colors.orange,
-        colors.green,
-        colors.gray
-    )
+    if not is_none(ME_BRIDGE.getTotalItemStorage()) then
+        guiProgressBar(
+                display,
+                2,
+                8,
+                "Storage",
+                display_x - 2,
+                3,
+                ME_BRIDGE.getUsedItemStorage(),
+                ME_BRIDGE.getTotalItemStorage(),
+                StorageColors
+        )
+    else
+        offline(display, StorageData, StorageColors)
+    end
     sleep(0.75)
 end
--- api/gui.lua
+++ api/gui.lua
@@ -1,60 +1,48 @@
-require("isa")
-
-display = peripheral.find("monitor")
-display_x, display_y = display.getSize()
-
-
-function guiClear()
+function guiClear(display)
     display.setBackgroundColor(colors.black)
     display.clear()
     display.setCursorPos(1, 1)
     display.setTextScale(1)
 end
 
-
-function guiDrawText(x, y, text, color_text, color_bg)
-    display.setBackgroundColor(color_bg)
-    display.setTextColor(color_text)
+function guiDrawText(display, x, y, text, text_color, bg_color)
+    display.setBackgroundColor(bg_color)
+    display.setTextColor(text_color)
     display.setCursorPos(x, y)
     display.write(text)
 end
 
-
-function guiDrawLine(x, y, length, size, color_bar)
-    for y_pos = y, y+size-1 do
-        display.setBackgroundColor(color_bar)
+function guiDrawLine(display, x, y, length, size, bar_color)
+    for y_pos = y, y + size - 1 do
+        display.setBackgroundColor(bar_color)
         display.setCursorPos(x, y_pos)
         display.write(string.rep(" ", length))
     end
 end
 
+function guiGetColorByLevel(percent_progress, gui_colors)
+    for _, condition in pairs(gui_colors.levels) do
+        if percent_progress <= condition.percent then
+            return condition.color
+        end
+    end
+end
 
-function guiProgressBar(x, y, name, length, size, min_value, max_value, color_bar_default, color_bar_warning, color_bar_danger, color_bg)
-    local bar_size = math.floor((min_value/max_value)*length)
-    local percent_progress = math.floor((min_value/max_value)*100)
+function guiProgressBar(display, x, y, name, length, size, min_value, max_value, gui_colors)
+    local display_x, display_y = display.getSize()
+    local bar_size = math.floor((min_value / max_value) * length)
+    local percent_progress = math.floor((min_value / max_value) * 100)
+    local text = ("%s %d%%"):format(name, percent_progress)
+    local bar_color = guiGetColorByLevel(percent_progress, gui_colors)
 
-    if is_nan(percent_progress) then
-        guiClear()
+    guiDrawLine(display, x, y, length, size, gui_colors.background_color)
+    guiDrawLine(display, x, y, bar_size, size, bar_color)
+
+    if bar_size > display_x / 2 + #text / 2 then
+        guiDrawText(display, display_x / 2 - #text / 2 + 2, y + size / 2, text, gui_colors.text_color, bar_color)
+    elseif bar_size > #text then
+        guiDrawText(display, (x + bar_size) - #text, y + size / 2, text, gui_colors.text_color, bar_color)
     else
-        local text = ("%s %d%%"):format(name, percent_progress)
-
-        color_bar = color_bar_default
-
-        if percent_progress >= 95 then
-            color_bar = color_bar_danger
-        elseif percent_progress >= 80 then
-            color_bar = color_bar_warning
-        end
-
-        guiDrawLine(x, y, length, size, color_bg)
-        guiDrawLine(x, y, bar_size, size, color_bar)
-
-        if bar_size > display_x/2+#text/2 then
-            guiDrawText(display_x/2-#text/2+2, y+size/2, text, colors.black, color_bar)
-        elseif bar_size > #text then
-            guiDrawText((x+bar_size)-#text, y+size/2, text, colors.black, color_bar)
-        else
-            guiDrawText(display_x/2-#text/2+2, y+size/2, text, colors.black, color_bg)
-        end
+        guiDrawText(display, display_x / 2 - #text / 2 + 2, y + size / 2, text, gui_colors.text_color, gui_colors.background_color)
     end
 end
